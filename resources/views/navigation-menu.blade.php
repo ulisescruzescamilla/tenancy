a279c75781760902646c9e3583fab861800796dd
Ulises Escamilla <ulises@escamilla.dev>
2023-09-02 00:25:01 -0600

    Add Tenants index page

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,3 +1,18 @@
+@php
+    $links = [
+        [
+            'url' => route('dashboard'),
+            'label' => "Panel de control",
+            'active' => request()->routeIs('dashboard')
+        ],
+        [
+            'url' => route('tenants.index'),
+            'label' => "Inquilinos",
+            'active' => request()->routeIs('tenants.index')
+        ],
+    ];
+
+@endphp
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -10,12 +25,15 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
+                @foreach ($links as $link)
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-nav-link href="{{ $link['url'] }}" :active="$link['active']">
+                            {{ $link['label'] }}
+                        </x-nav-link>
+                    </div>
+                @endforeach
+
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -139,9 +157,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+            @foreach ($links as $link)
+                <x-responsive-nav-link href="{{ $link['url'] }}" :active="$link['active']">
+                    {{ $link['label'] }}
+                </x-responsive-nav-link>
+            @endforeach
         </div>
 
         <!-- Responsive Settings Options -->
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TenancyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +26,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::resource('/tenants', TenancyController::class);
 });
